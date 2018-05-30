c0b6b70216837513198a0ba3dd174860e42c5754
llin <llin@shutterfly.com>
2018-05-29 23:42:48 -0700

    746-min-cost-climbing-stairs

---
+++ 674_MinCostClimbingStairs_review.js
@@ -0,0 +1,13 @@
+/**
+ * Created by llin on 5/29/18.
+ */
+
+//Needs review - could not figure that out, directly copy from the discussion
+var minCostClimbingStairs = function(cost) {
+    var len = cost.length;
+    var minCost = cost;
+    for (var i=2; i<len; i++) {
+        minCost[i] += Math.min(minCost[i-1], minCost[i-2]);
+    }
+    return Math.min(minCost[len-1], minCost[len-2]);
+};
