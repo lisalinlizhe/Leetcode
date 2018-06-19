27256cd2ebc578fb6d66770cb41255008deae842
lisalinlizhe <lisalizhe@gmail.com>
2018-06-19 00:30:41 -0700

    test

--- BitManipulation/Easy/342_PowerOfFour.js
+++ BitManipulation/Easy/342_PowerOfFour.js
@@ -1,7 +1,7 @@
 /**
  * Created by lizhelin on 18/6/18.
  */
-// is power of 2 and can divide 3.
+//if is power of 2 and can divide 3.
 var isPowerOfFour = function(num) {
   if(num <= 0) return false;
   var isPowerOfTwo = (num & (num - 1)) === 0;
