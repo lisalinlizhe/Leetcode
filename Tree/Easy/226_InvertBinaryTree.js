224f3273606ede50561f2c24ab2c6e6d7cf19249
lisalinlizhe <lisalizhe@gmail.com>
2018-06-20 22:16:23 -0700

    226

--- Tree/Easy/226_InvertBinaryTree.js
+++ Tree/Easy/226_InvertBinaryTree.js
@@ -1,3 +1,13 @@
 /**
  * Created by lizhelin on 18/6/20.
  */
+var invertTree = function(root) {
+  if(root === null) return null;
+  if(root.left || root.right) {
+    var temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+  }
+  else return root;
+  return root;
+};
