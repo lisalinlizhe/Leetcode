f9009df208804393e63ac57a348523b9919af613
lisalinlizhe <lisalizhe@gmail.com>
2018-06-29 21:37:25 -0700

    495

---
+++ Array/Medium/495_TeemoAttacking.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lizhelin on 18/6/29.
+ */
+if(timeSeries.length === 0) return 0;
+var l = timeSeries.length;
+var res = duration;
+for(var i = 0; i < l - 1; i++) {
+  if(timeSeries[i + 1] - timeSeries[i] >= duration) res += duration;
+  else res += timeSeries[i + 1] - timeSeries[i];
+}
+return res;
+};
