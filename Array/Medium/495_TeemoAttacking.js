fbf5fc6a1516361358aa793f53ee46de4462c773
lisalinlizhe <lisalizhe@gmail.com>
2018-07-01 21:09:57 -0700

    adjust

--- Array/Medium/495_TeemoAttacking.js
+++ Array/Medium/495_TeemoAttacking.js
@@ -1,12 +1,13 @@
 /**
  * Created by lizhelin on 18/6/29.
  */
-if(timeSeries.length === 0) return 0;
-var l = timeSeries.length;
-var res = duration;
-for(var i = 0; i < l - 1; i++) {
-  if(timeSeries[i + 1] - timeSeries[i] >= duration) res += duration;
-  else res += timeSeries[i + 1] - timeSeries[i];
-}
-return res;
+var findPoisonedDuration = function(timeSeries, duration) {
+  if(timeSeries.length === 0) return 0;
+  var l = timeSeries.length;
+  var res = duration;
+  for(var i = 0; i < l - 1; i++) {
+    if(timeSeries[i + 1] - timeSeries[i] >= duration) res += duration;
+    else res += timeSeries[i + 1] - timeSeries[i];
+  }
+  return res;
 };
