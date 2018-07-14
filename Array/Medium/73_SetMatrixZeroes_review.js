c33524302b4176528836bc926d0175eb49d13c02
lisalinlizhe <lisalizhe@gmail.com>
2018-07-13 21:59:41 -0700

    follow-up

--- Array/Medium/73_SetMatrixZeroes_review.js
+++ Array/Medium/73_SetMatrixZeroes_review.js
@@ -28,3 +28,27 @@ var setZeroes = function(matrix) {
     }
   }
 };
+
+
+// faster one with o(1) space
+//var setZeroes = function(matrix) {
+//  var row = matrix.length;
+//  var col = matrix[0].length;
+//  var col0 = -1;
+//  for(var i = 0; i < row; i++) {
+//    if(matrix[i][0] === 0) col0 = 0;
+//    for(var j = 1 ; j < col ; j++) {
+//      if(matrix[i][j] === 0) {
+//        matrix[i][0] = matrix[0][j] = 0;
+//      }
+//    }
+//  }
+//  for(var i = row - 1; i >= 0 ; i--) {
+//    for(var j = col - 1; j >= 1; j--) {
+//      if(matrix[i][0] === 0 || matrix[0][j] === 0) {
+//        matrix[i][j] = 0;
+//      }
+//    }
+//    if(col0 === 0) matrix[i][0] = 0;
+//  }
+//};
