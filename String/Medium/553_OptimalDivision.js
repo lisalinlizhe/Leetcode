0a8ce2b67218baee4a464cda769f2181a6d30766
lisalinlizhe <lisalizhe@gmail.com>
2018-07-31 21:49:25 -0700

    553

---
+++ String/Medium/553_OptimalDivision.js
@@ -0,0 +1,15 @@
+/**
+ * Created by lizhelin on 18/7/31.
+ */
+//这题没啥意思？
+var optimalDivision = function(nums) {
+  const n = nums.length
+  if (n < 1) return ''
+  if (n === 1) return '' + nums[0]
+  if (n === 2) return nums[0] + '/' + nums[1]
+  let result = nums[0] + '/(' + nums[1]
+  for (let i = 2; i < n; i++) {
+    result += '/' + nums[i]
+  }
+  return result + ')'
+}
