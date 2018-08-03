8e7049cc1c9416a0c48ca0faba4f829956de63b7
lisalinlizhe <lisalizhe@gmail.com>
2018-08-03 12:01:07 -0700

    678

---
+++ String/Medium/678_ValidParenthesisString_review.js
@@ -0,0 +1,28 @@
+/**
+ * Created by lizhelin on 18/8/3.
+ */
+var checkValidString = function(s) {
+  var sum = 0;
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '*') {
+      sum ++;
+    } else {
+      sum --;
+    }
+    if (sum < 0) {
+      return false
+    }
+  }
+  sum = 0;
+  for (var i = s.length - 1; i >=0; i--) {
+    if (s[i] === ')' || s[i] === '*') {
+      sum ++;
+    } else {
+      sum --;
+    }
+    if (sum < 0) {
+      return false;
+    }
+  }
+  return true;
+}
