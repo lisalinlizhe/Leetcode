625ac09eceb7b202ecf943e1ef98881873a3a8cd
lisalinlizhe <lisalizhe@gmail.com>
2018-08-10 11:34:43 -0700

    722

---
+++ String/Medium/722_RemoveComments_review.js
@@ -0,0 +1,34 @@
+/**
+ * Created by lizhelin on 18/8/10.
+ */
+var removeComments = function(paths) {
+  var res = []
+  var str = "";
+  var mode = false;
+  for(var i = 0; i < paths.length; i++) {
+    var s = paths[i];
+    for(var j = 0; j < s.length; j++) {
+      if(mode) {
+        if(s[j] === '*' && j < s.length - 1 && s[j + 1] === '/' ){
+          mode = false;
+          j++;
+        }
+      }
+      else{
+        if(s[j] === '/' &&  j < s.length - 1 && s[j + 1] === '/') {
+          break;
+        }
+        else if(s[j] === '/' &&  j < s.length - 1 && s[j + 1] === '*'){
+          mode = true;
+          j++;
+        }
+        else str += s[j];
+      }
+    }
+    if(!mode && str.length > 0) {
+      res.push(str);
+      str = "";
+    }
+  }
+  return res;
+};
