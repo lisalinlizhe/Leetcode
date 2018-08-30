2275dff49484d175b868b5861004271e3550f287
lisalinlizhe <lisalizhe@gmail.com>
2018-08-29 23:40:19 -0700

    554

---
+++ HashTable/Medium/554_BrickWall_review.js
@@ -0,0 +1,23 @@
+/**
+ * Created by lizhelin on 18/8/29.
+ */
+//so close, last for loop cannot figure out
+var leastBricks = function(wall) {
+  var map = {};
+  var res = wall.length;
+  for(var i = 0; i < wall.length; i++) {
+    var sum = 0;
+    for(var j = 0; j < wall[i].length - 1; j++) {
+      sum += wall[i][j];
+      if(map[sum] !== undefined) {
+        map[sum]++;
+      }
+      else map[sum] = 1;
+    }
+  }
+  var keys = Object.keys(map);
+  for(var k = 0; k < keys.length; k++) {
+    res = Math.min(res, wall.length - map[keys[k]]);
+  }
+  return res;
+};  
